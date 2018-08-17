5eeccfb58d366b03fd5644ae414747686ccd7775
Jascha Beste <jascha.beste@student.hpi.de>
2018-08-18 01:31:31 +0200

    Initialize client vue project and server express project

---
+++ client/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>This page will allow followers of Lancemenot to register for his Tournament Tuesdays</p>
+  </div>
+</template>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Welcome to the work in progress signup
+     page for lancemenots Tournament Tuesday"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
